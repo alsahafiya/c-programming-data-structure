1fa3359dd66b1aaa546c8152eb05b161f501681a
alsahafiya <alsahafiya7483@gmail.com>
2024-09-12 16:23:18 +0530

    Add files via upload

---
+++ LINKED LIST 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int bibi(int a,int b,int c);
+int main()
+{ int a,b,c;
+  printf("bibi maryam\n");
+  printf("syed fairoz\n");
+  printf("syeda ameena noorie\n");
+  }
+int bibi(int a,int b,int c){
+  printf("%d",&a);
+  printf("%d",&b);
+  printf("%d",&c);
+}
+
+
