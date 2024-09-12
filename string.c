309337b3e4c93506e48080e7d01b24c958b87cb8
alsahafiya <alsahafiya7483@gmail.com>
2024-09-12 16:24:32 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{ int n,i;
+char str[50];
+printf("enter n");
+scanf("%d",&n);
+printf("enter string");
+scanf("%s",str);
+for(i=0;i<=n;i++)
+ printf("%c",str[i]);
+}
+  
