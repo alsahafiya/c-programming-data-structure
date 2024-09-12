9ce9e65363d92ab04e54811dc84938934901873e
alsahafiya <alsahafiya7483@gmail.com>
2024-09-12 16:24:51 +0530

    Add files via upload

---
+++ 1 D deletion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int pos=2,value=10;
+  int arr[5]={1,2,3,4,5};
+  for(int i=4;i>=pos;i--){
+  arr [i+1]= arr[i];
+    }
+    arr[pos]=value;
+ 
+  printf("%d",arr[pos]);
+ 
+}
+
